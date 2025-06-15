88c1bfb6970effe49fd9fb42cf1d556f70a171db
MwiseHeritier <mwisehertier>
2025-06-15 17:56:19 +0200

    Make add to cart button interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary 
+      js-add-to-cart" data-product-name= "${product.id}">
         Add to Cart
       </button>
     </div>
@@ -54,3 +55,62 @@ products.forEach((product) => {
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+
+    /* 
+      Steps to add product in a cart
+      ------------------------------
+      1. check if the product is already in the cart.
+      2. if it is in the cart, increase the quantity.
+      3. if it is not in the cart, add it to the cart.
+    */
+    
+    let matchingItem; // we use this variable to save the product which is already  in the cart.
+
+    cart.forEach((item) => { // item will contain the name and quantity of product.
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem){
+      matchingItem.quantity += 1;
+    }
+
+    else{
+      cart.push({
+      productId: productId,
+      quantity: 1
+    });
+    }
+
+   
+    
+    console.log(cart)
+    });
+  });
+
+
+
+
+ /*
+   
+    Data attribute
+    --------------
+    -> is just another HTML attribute.
+    -> allows us to attach/store any information to an element.
+    -> have to start with "data-", then give it any name.
+    -> Helps to know which product we want to add in container (like cart in this example)
+
+    Dataset
+    -------
+    dataset is a property of an HTML element that gives you access
+    to all the data-* attributes set on that element. These data-* attributes
+    are used to store extra information directly on HTML elements without using classes or IDs.
+    The attribute names are converted from kebab-case (data-user-id) to camelCase (dataset.userId
+
+  */
