3a3896b2a7e59af1946ef42c609ab7fd70bb1a53
MwiseHeritier <mwisehertier>
2025-06-16 19:49:04 +0200

    Make tittle of checkout.html interactive.

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -116,5 +116,18 @@ document.querySelectorAll('.js-delete-link') // we selected all delete link on t
      
       const container = document.querySelector(`.js-cart-item-container-${productId}`);
       container.remove();
-    })
+      updateCartQuantity ();
+    });
   });
+
+  function updateCartQuantity() {
+    let cartQuantity = 0;
+    cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${cartQuantity} items`;
+  }
+  
+  updateCartQuantity();
